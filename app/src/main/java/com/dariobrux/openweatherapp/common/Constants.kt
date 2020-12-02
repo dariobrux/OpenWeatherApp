cb69dd88ea3bc81bf5337b25944ef0acba550f11
Dario Bruzzese <dariobrux@gmail.com>
2020-12-02 22:20:31 +0100

    Use https

--- app/src/main/java/com/dariobrux/openweatherapp/common/Constants.kt
+++ app/src/main/java/com/dariobrux/openweatherapp/common/Constants.kt
@@ -7,10 +7,10 @@ package com.dariobrux.openweatherapp.common
  */
 object Constants {
 
-    const val BASE_URL = "http://api.openweathermap.org/"
+    const val BASE_URL = "https://api.openweathermap.org/"
     const val APP_ID = "56caf2dbe33614b8b355e454b6f6649e"
 
-    const val ICON_BASE_URL = "http://openweathermap.org/"
+    const val ICON_BASE_URL = "https://openweathermap.org/"
     const val PATH_NETWORK_ICON = "img/wn/"
     const val ICON_FORMAT = "@2x.png"
 }
