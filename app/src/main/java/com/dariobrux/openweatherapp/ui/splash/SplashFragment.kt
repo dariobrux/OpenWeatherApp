e68bd253e96119adcdb7a0b9887d56df2b1e34b9
Dario Bruzzese <dariobrux@gmail.com>
2020-12-04 13:00:13 +0100

    Remove splash unused code

--- app/src/main/java/com/dariobrux/openweatherapp/ui/splash/SplashFragment.kt
+++ app/src/main/java/com/dariobrux/openweatherapp/ui/splash/SplashFragment.kt
@@ -11,9 +11,6 @@ import com.dariobrux.openweatherapp.R
 import com.dariobrux.openweatherapp.common.extension.toMainActivity
 import com.dariobrux.openweatherapp.databinding.FragmentSplashBinding
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.cancel
 
 /**
  *
@@ -29,12 +26,6 @@ class SplashFragment : Fragment() {
      * View binder. Destroy it in onDestroyView avoiding memory leaks.
      */
     private var binding: FragmentSplashBinding? = null
-
-    /**
-     * The Coroutine Scope in which apply the splash timer. Starts in
-     * onResume and cancels in onPause.
-     */
-    private val activityScope = CoroutineScope(Dispatchers.Main)
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         binding = FragmentSplashBinding.inflate(inflater, container, false)
@@ -60,11 +51,6 @@ class SplashFragment : Fragment() {
                 NavHostFragment.findNavController(requireParentFragment()).navigate(R.id.action_splashFragment_to_locationFragment)
             }.start()
         }
-    }
-
-    override fun onPause() {
-        activityScope.cancel()
-        super.onPause()
     }
 
     override fun onDestroyView() {
